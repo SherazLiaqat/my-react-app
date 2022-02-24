d53f7937da7b7ff54f7ebc70077f958cf8786e71
SherazLiaqat <78857931+SherazLiaqat@users.noreply.github.com>
2022-02-25 02:06:05 +0500

    Delete project1 directory

--- project1/src/Screens/Project2/BackDrop.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function BackDrop() {
-  return (
-    <div className='backdrop'/>
-  )
-}
--- project1/src/Screens/Project2/Meetup/AllMeetup.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function AllMeetup() {
-  return (
-    <div> <h1>helo</h1></div>
-   
-  )
-}
